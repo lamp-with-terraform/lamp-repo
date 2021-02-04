710789a1aa20656f2c629e379bffef80608f48ac
armengev <64475505+armengev@users.noreply.github.com>
2021-02-04 19:41:46 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,28 @@
+####ASG-vars#####
 variable "key_name" {}
+
 variable "public_key_path" {}
 variable "region" {}
-variable "vpc_cidr" {}
-variable "subnet_one_cidr" {}
-variable "subnet_two_cidr" {
-    type = "list"
-}
 variable "route_table_cidr" {}
+
 variable "web_ports" {
-    type = "list"
+  type = "list"
 }
+
 variable "db_ports" {
-    type = "list"
+  type = "list"
 }
 
+variable "db_private_subnet_cidr" {
+  type = "list"
+}
 
+variable "vpc_cidr" {}
+
+variable "public_subnet_cidr" {
+  type = "list"
+}
+
+variable "app_private_subnet_cidr" {
+  type = "list"
+}
