926ab0e06ab58ec2fdb773769e7bb56a37bee7c8
armengev <64475505+armengev@users.noreply.github.com>
2021-02-04 19:36:39 +0400

    Update outputs.tf

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -1,17 +1,32 @@
-#-----networking/outputs.tf
+##############networking-outputs############
+output "db_security_group" {
+  value = "${aws_security_group.db_security_group.id}"
+}
 
-output "myvpc_public_subnet" {
-  value = "${aws_subnet.myvpc_public_subnet.id}"
+output "db_subnet_group_name" {
+  value = "${aws_db_subnet_group.db_subnet_group.name}"
 }
 
 output "web_security_group" {
   value = "${aws_security_group.web_security_group.id}"
 }
 
-output "db_security_group" {
-  value = "${aws_security_group.db_security_group.id}"
+output "vpc_id" {
+  value = "${aws_vpc.lamp_vpc.id}"
 }
 
-output "my_database_subnet_group" {
-  value = "${aws_db_subnet_group.my_database_subnet_group.id}"
+output "private_subnet1" {
+  value = "${aws_subnet.lamp_vpc_app_private_subnet1.id}"
+}
+
+output "private_subnet2" {
+  value = "${aws_subnet.lamp_vpc_app_private_subnet2.id}"
+}
+
+output "public_subnet1" {
+  value = "${aws_subnet.lamp_vpc_public_subnet1.id}"
+}
+
+output "public_subnet2" {
+  value = "${aws_subnet.lamp_vpc_public_subnet2.id}"
 }
