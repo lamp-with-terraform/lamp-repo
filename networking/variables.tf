1134d0aab0c261de4b80137237bcff432c7bc1dc
armengev <64475505+armengev@users.noreply.github.com>
2021-02-04 19:39:33 +0400

    Create variables.tf

---
+++ networking/variables.tf
@@ -0,0 +1,25 @@
+###################Network_var###############
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_cidr" {
+  type = "list"
+}
+
+variable "app_private_subnet_cidr" {
+  type = "list"
+}
+
+variable "db_private_subnet_cidr" {
+  type = "list"
+}
+
+variable "route_table_cidr" {}
+
+variable "db_ports" {
+  type = "list"
+}
+
+variable "web_ports" {
+  type = "list"
+}
