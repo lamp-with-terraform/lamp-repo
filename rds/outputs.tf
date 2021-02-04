052ea74692a18d42419a9f687b11bb97fa2536f1
armengev <64475505+armengev@users.noreply.github.com>
2021-02-04 19:40:15 +0400

    Update outputs.tf

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -1,4 +1,4 @@
 #output webserver and dbserver address
 output "db_server_address" {
-value = "${aws_db_instance.my_database_instance.address}"
+  value = "${aws_db_instance.lamp_database_instance.address}"
 }
