29f68d946f129a285fd152a5257a8df72a7df23e
armengev <64475505+armengev@users.noreply.github.com>
2021-02-04 19:35:00 +0400

    Create main.tf

---
+++ elb/main.tf
@@ -0,0 +1,22 @@
+######################ELB##########################
+
+# Creating application load balancer
+resource "aws_lb" "lamp_lb" {
+  name               = "lamplb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${var.my_web_security_group}"]
+  subnets            = ["${var.my_public_subnet1}", "${var.my_public_subnet2}"]
+}
+
+# Configuring listener and target group
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = "${var.my_lb_arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${var.my_tg_arn}"
+  }
+}
