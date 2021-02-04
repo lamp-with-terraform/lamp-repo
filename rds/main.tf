4d3c1b77a257f7407159778d177bbf41511b8829
armengev <64475505+armengev@users.noreply.github.com>
2021-02-04 19:39:57 +0400

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -1,20 +1,23 @@
+##############RDS##################
+
 #create aws mysql rds instance
-resource "aws_db_instance" "my_database_instance" {
-allocated_storage = 20
-storage_type = "gp2"
-engine = "mysql"
-engine_version = "5.7"
-instance_class = "db.t2.micro"
-port = 3306
-vpc_security_group_ids = ["${var.db_security_group}"]
-db_subnet_group_name = "${var.my_database_subnet_group}"
-name = "mydb"
-identifier = "mysqldb"
-username = "myuser"
-password = "mypassword"
-parameter_group_name = "default.mysql5.7"
-skip_final_snapshot = true
-tags = {
-Name = "my_database_instance"
-}
+resource "aws_db_instance" "lamp_database_instance" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  port                   = 3306
+  vpc_security_group_ids = ["${var.my_db_security_group}"]
+  db_subnet_group_name   = "${var.my_db_subnet_group_name}"
+  name                   = "mydb"
+  identifier             = "mysqldb"
+  username               = "lampuser"
+  password               = "lamppassword"
+  parameter_group_name   = "default.mysql5.7"
+  skip_final_snapshot    = true
+
+  tags = {
+    Name = "lamp_database_instance"
+  }
 }
