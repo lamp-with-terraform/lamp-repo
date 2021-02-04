ebd457980355db4851c04b4df7d337e831a62a49
armengev <64475505+armengev@users.noreply.github.com>
2021-02-04 19:34:31 +0400

    Create variables.tf

---
+++ asg/variables.tf
@@ -0,0 +1,8 @@
+################asg-variables#################
+variable "key_name" {}
+variable "public_key_path" {}
+variable "my_db_server_address" {}
+variable "my_web_security_group" {}
+variable "my_private_subnet1" {}
+variable "my_private_subnet2" {}
+variable "my_vpc_id" {}
