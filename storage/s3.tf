edb49a1655a28283d958bf39a0f80ec4a03eefe9
armengev <64475505+armengev@users.noreply.github.com>
2021-02-04 20:28:43 +0400

    Create s3.tf

---
+++ storage/s3.tf
@@ -0,0 +1,9 @@
+# Setting up region
+provider "aws" {
+  region = "us-east-1"
+}
+#Crating bucket for tfstate
+resource "aws_s3_bucket" "tf_state_bucket" {
+  bucket = "my-tf-state-bucket-0123456789"
+  acl    = "private"
+}
