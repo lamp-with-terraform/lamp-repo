be2c95557f291dd171710db2ea97f36742665dde
armengev <64475505+armengev@users.noreply.github.com>
2022-07-12 17:40:41 +0400

    Update main.tf

--- rds/main.tf
+++ rds/main.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "lamp_database_instance" {
   storage_type           = "gp2"
   engine                 = "mysql"
   engine_version         = "5.7"
-  instance_class         = "db.t2.micro"
+  instance_class         = "db.t2.medium"
   port                   = 3306
   vpc_security_group_ids = ["${var.my_db_security_group}"]
   db_subnet_group_name   = "${var.my_db_subnet_group_name}"
