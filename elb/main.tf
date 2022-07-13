06783528133bf217edffc2575b4ccd86f9ab0cbc
armengev <64475505+armengev@users.noreply.github.com>
2022-07-13 11:23:50 +0400

    Update main.tf

--- elb/main.tf
+++ elb/main.tf
@@ -14,7 +14,6 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = "${aws_lb.lamp_lb.arn}"
   port              = "80"
   protocol          = "HTTP"
-
   default_action {
     type             = "forward"
     target_group_arn = "${var.my_tg_arn}"
