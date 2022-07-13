efcc6e3c2ad0d18e09621839d9c93a196a8e0365
armengev <64475505+armengev@users.noreply.github.com>
2022-07-13 11:28:59 +0400

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -7,7 +7,7 @@ data "aws_availability_zones" "availability_zones" {}
 resource "aws_vpc" "lamp_vpc" {
   cidr_block = "${var.vpc_cidr}"
 
-  #enable_dns_hostnames = true
+#enable_dns_hostnames = true
   tags = {
     Name = "lamp-vpc"
   }
